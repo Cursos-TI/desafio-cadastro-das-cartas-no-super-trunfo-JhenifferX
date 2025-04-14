1ae949ae48b333e3352a297a5403ae6ebc0fb4b8
JhenifferX <jhenifferx08@gmail.com>
2025-04-14 03:03:18 +0000

    novo commit

--- Xadrez.c
+++ Xadrez.c
@@ -57,8 +57,9 @@ int main(){
   printf("PiB: %.2f bilhões de reais\n", pib);
   printf("Números de Pontos Tuísticos: %d\n", numerosdepontosturisticos);
 
+// Para Carta 1
 
-// B
+// BA
 // A01
 // Salvador
 // 14850513
@@ -66,52 +67,18 @@ int main(){
 // 115.9
 // 13
 
+//Para a Carta 2
+
+//TO
+//B02
+//Palmas
+// 1.577.342
+// 277.423,62 km²
+// 3 080,4
+// 7
+
  // Carta 1 criada com sucesso.
 
 
 return 0;
-
-}
-
-#include <stdio.h>() {
-
-    // Declarando Variáveis
-
-    char carta2[10];
-    char estado[20];
-    char codigo[10];
-    char nomedacidade[50];
-    int populacao;
-    float area;
-    float pib;
-    int numerosdepontosturisticos;
-
-    //Inicializando as variáveis
-
-    printf("Digite o estado ");
-    scanf("%s", estado);
-
-    printf("Digite o código: ");
-    scanf("%s",codigo);
-
-    printf("Digite o nome da cidade: ");
-    scanf("%s", nomedacidade);
-
-    printf("Digite a populção: ");
-    scanf("%d", &populacao);
-
-    printf("Digite a área: ");
-    scanf("%f", &area);
-
-    printf("Digite o PIB: ");
-    scanf("%f", &pib);
-
-    printf("Digite o número de pontos turisticos);
-    scanf("%d", &numerosdeponstosturisticos);
-
-
-
-return 0;
-
- }
 
