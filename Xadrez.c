62022e754cc041eb79a61d0537fdf95dd796485f
JhenifferX <jhenifferx08@gmail.com>
2025-04-14 03:17:50 +0000

    corrigindo erros

--- Xadrez.c
+++ Xadrez.c
@@ -84,3 +84,60 @@ int main(){
 return 0;
 
 }
+
+#include <stdio.h>
+
+int main(){
+
+  //Declarando Variáveis
+
+  char carta1[10];
+  char estado[20];
+  char codigo[10];
+  char nomedacidade[50];
+  int populacao;
+  float area;
+  float pib;
+  int numerosdepontosturisticos;
+
+
+  // Inicializando as variáveis
+
+  printf("Digite o estado: ");
+  scanf("%s", &estado);
+
+
+  printf("Digite o código: ");
+  scanf("%s", &codigo);
+  
+
+  printf("Digite o nome da cidade: ");
+  scanf("%s", &nomedacidade);
+
+
+  printf("Digite a população: ");
+  scanf("%d", &populacao);
+
+  printf("Digite a área: ");
+  scanf("%f", &area);
+
+  printf("Digite o PIB: ");
+  scanf("%f", &pib);
+
+  printf("Digite o números de pontos turísticos: ");
+  scanf("%d" , &numerosdepontosturisticos);
+
+  // Exibindo os dados fornecidos
+
+  printf("Carta1: %s\n", carta1);
+  printf("Estado: %s\n", estado);
+  printf("Código: %s\n", codigo);
+  printf("Nome Da Cidade: %s\n", nomedacidade);
+  printf("População: %d\n", populacao);
+  printf("Àrea: %.2f km²\n", area);
+  printf("PiB: %.2f bilhões de reais\n", pib);
+  printf("Números de Pontos Tuísticos: %d\n", numerosdepontosturisticos);
+
+  return 0;
+
+}
